6c3f475b5a0ed650b142d0b3f0565e2b608dac6f
Heather-Ridgill <Heather.Ridgill@gmail.com>
2020-07-19 18:42:47 -0700

    Completed and passed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,18 @@
+# Positive and Negative
+
+# For an input list of integers, we wish to know which positive numbers
+# have corresponding negative numbers in the list.
+
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    result = []
+# abs = keyword :) 
+    for num in a:
+        absolute = abs(num)
+        if cache.get(absolute) is not None:
+            result.append(absolute)
+        else:
+            cache[absolute] = absolute
 
     return result
 
